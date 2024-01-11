1e9f83846bf9cad429fd7bf64c57c7e08ad37741
Jack O'Brien <112610774+jadyobri@users.noreply.github.com>
2024-01-10 20:36:48 -0800

    Add files via upload

---
+++ nameAndSentences.py
@@ -0,0 +1,18 @@
+# Print function for name
+def PrintName():
+    name = "Jack O'Brien"
+    print(name)
+
+# Print function for sentences.
+def PrintSentences():
+    sentence = "\nMy favorite movie is Seven Samuari, which is a Japanese film made in 1954.\nMy favorite food is my mom's special type of lasngua that the person she got the recipe from calls Mexican lasgnua."
+    print(sentence)
+
+# Prints everything out
+def main():
+    PrintName()
+    PrintSentences()
+
+# Executes the main program when script is run
+if __name__=="__main__":
+    main()
